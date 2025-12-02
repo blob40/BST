22b3b939f6115f5d6db1dea7e42b24e58bbf8354
blob40 <alexandrialax40@gmail.com>
2025-12-02 18:17:07 +0000

    start

---
+++ BST.java
@@ -0,0 +1,26 @@
+class BST {
+
+    public BST()
+    {
+         root = null;
+    }
+
+   
+
+    void insert(int key){
+
+    }
+
+    boolean search(int key){
+
+    }
+
+    int remove(int key){
+
+    }
+
+    public String toString(){
+        
+    }
+
+}
